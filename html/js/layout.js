b5931b5430f82f9ab0eff1566353a113f0dbcbc7
q123wer2002 <q123wer2002>
2017-01-08 16:25:58 +0800

    change shopping page

--- html/js/layout.js
+++ html/js/layout.js
@@ -3,21 +3,21 @@ var vegefruit66 = angular.module('vegefruit66', []);
 //controller
 vegefruit66.controller('mainController', function($scope, $rootScope){
 	//header
-	$rootScope.header = {
-		title : "蔬果溜溜",
-		metas : [
-			{name : "charset",content : "UTF-8"},
-			{name : "description",content : "vegefruit66, 蔬果溜溜"},
-			{name : "keywords",content : "vegefruit66, 蔬果溜溜"},
-			{name : "viewport",content : "width=device-width, initial-scale=1, maximum-scale=1"}
-		],
-	};
+		$rootScope.header = {
+			title : "蔬果溜溜",
+			metas : [
+				{name : "charset",content : "UTF-8"},
+				{name : "description",content : "vegefruit66, 蔬果溜溜"},
+				{name : "keywords",content : "vegefruit66, 蔬果溜溜"},
+				{name : "viewport",content : "width=device-width, initial-scale=1, maximum-scale=1"}
+			],
+		};
 
-	$rootScope.includePages = {
-		top : "./templates/top.html",
-		body : "", //depends on page
-		footer : "./templates/footer.html",
-	};
+		$rootScope.includePages = {
+			top : "./templates/top.html",
+			body : "", //depends on page
+			footer : "./templates/footer.html",
+		};
 
 		//script
 		var objPreLoadScript = {
@@ -33,18 +33,18 @@ vegefruit66.controller('mainController', function($scope, $rootScope){
 		}
 
 	//menu list
-	$rootScope.objMenuList = {
-		vegeinfo : {name:"蔬果小學堂", link:"vegeinfo.html", isSelected:false, isShown:false},
-		farmerTalk : {name:"農民都說讚", link:"farmerTalk.html", isSelected:false, isShown:false},
-		friendShop : {name:"合作商家", link:"friendShop.html", isSelected:false, isShown:false},
-		activity : {name:"活動專欄", link:"activity.html", isSelected:false, isShown:true}
-	};
+		$rootScope.objMenuList = {
+			vegeinfo : {name:"蔬果小學堂", link:"vegeinfo.html", isSelected:false, isShown:false},
+			farmerTalk : {name:"農民都說讚", link:"farmerTalk.html", isSelected:false, isShown:false},
+			friendShop : {name:"合作商家", link:"friendShop.html", isSelected:false, isShown:false},
+			activity : {name:"活動專欄", link:"activity.html", isSelected:false, isShown:true}
+		};
 
-	$rootScope.StyleTargetMenu = function(objMenu){
-		if( $rootScope.currentLink == objMenu.link ){
-			objMenu.isSelected = true;
+		$rootScope.StyleTargetMenu = function(objMenu){
+			if( $rootScope.currentLink == objMenu.link ){
+				objMenu.isSelected = true;
+			}
 		}
-	}
 });
 
 //directive
--- html/js/shopping.js
+++ html/js/shopping.js
@@ -2,31 +2,56 @@
 vegefruit66.controller('shoppingController', function($scope,$rootScope){
 	$rootScope.currentLink = "shopping.html";
 
-	$scope.objOptions = {
+	$scope.fnGetNextDeliveryDate = function(nDayOfWeek){
+		//0~6
+		var dateToday = new Date();
+
+		//this week would delivery
+		dateToday.setDate(dateToday.getDate() + (nDayOfWeek-1-dateToday.getDay()+7)%7+1);
+
+		return (dateToday.getMonth()+1) + "/" + dateToday.getDate();
+	}
+
+	//video
+		$scope.isShowVideo = false;
+		$scope.fnChangeShowVideo = function(){
+			$scope.isShowVideo = !$scope.isShowVideo;
+			jQuery(".videoPage").css('display','block');
+		}
+	//orange
+	$scope.isSelected = false;
+	$scope.nOrangeDeliveryDay = 3; //Wednesday
+	$scope.objOrangeOption = {
 		ForUs : {
-			title : "自己吃",
+			title : "蜜柑饗宴",
 			price : 599,
-			detail : "茂谷蜜柑(有機栽種)，25A/27A/30A混裝，一箱10士5%台斤(約25顆)。註:外觀較差，保值期較短，但較甜，數量有限",
+			detail : "茂谷蜜柑(有機栽種)，25A/27A混裝，一箱10士5%台斤(25~27顆)。註:外觀較差，保值期較短，但較甜，數量有限。",
 			photo : "option1.jpg",
+			count : 0,
 		},
 		ForPresent : {
 			title : "拌手禮",
 			price : 799,
-			detail : "茂谷蜜柑25A/27A/30A混裝，一箱10士5%台斤(約25顆)。塑膠套封裝可存放一、兩個月，但建議盡早食用，風味較佳。",
+			detail : "茂谷蜜柑25A/27A混裝，一箱10士5%台斤(25~27顆)。塑膠套封裝可存放一、兩個月，但建議盡早食用，風味較佳。",
 			photo : "option2.jpg",
+			count : 0,
 		},
 		ForEmpire : {
 			title : "尊榮不凡",
 			price : 1099,
 			detail : "茂谷蜜柑30A頂級禮盒裝，一箱9士5%台斤(約18顆)。塑膠套封裝可存放一、兩個月，但建議盡早食用，風味較佳。",
 			photo : "option3.jpg",
+			count : 0,
 		},
 	};
+	$scope.fnCountOrange = function(){
+		for( var option in $scope.objOrangeOption ){
+			if( $scope.objOrangeOption[option].count != 0 ){
+				$scope.isSelected = true;
+				return;
+			}
+		}
 
-	//video
-	$scope.isShowVideo = false;
-	$scope.fnChangeShowVideo = function(){
-		$scope.isShowVideo = !$scope.isShowVideo;
-		jQuery(".videoPage").css('display','block');
+		$scope.isSelected = false;
 	}
 });
