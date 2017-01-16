9c79f65412196407e39aeab707737bad202bead3
q123wer2002 <q123wer2002>
2017-01-17 00:27:17 +0800

    add nodemailer

---
+++ module/EmailManager.js
@@ -0,0 +1,49 @@
+'use strict'
+
+//lib
+var EmailModule = require('nodemailer');
+//end lib
+
+//setting
+var mailTransporter = EmailModule.createTransport("SMTP",{
+    service: 'Gmail',
+    auth: {
+        user: 'healthyvegetable1612@gmail.com',
+        pass: 'vegetable1277'
+    }
+});
+
+var objOptions = {
+	from : "蔬果溜溜<healthyvegetable1612@gmail.com>", //sender
+	to : "", //receiver
+	cc : "", //cc
+	bcc : "", //secert cc
+	subject : "",
+	text : "", //plaintext body
+	html : "此信件由系統發出<hr><br>",
+	attachments : [],
+	/*
+	{filename : "",content:""}
+	{filename : "",path:""}
+	*/
+};
+//end setting
+var mailModule = {};
+mailModule.fnSendMail = function(options,fnCallback){
+	objOptions.to = options.receiver;
+	objOptions.subject = options.topic;
+	objOptions.html += options.content;
+
+	mailTransporter.sendMail(objOptions,function(err,info){
+		 if(err){
+		 	return fnCallback( "error" + err);
+		 }
+
+		 fnCallback(false,info);
+	});
+}
+
+
+
+//http://ithelp.ithome.com.tw/articles/10160766
+module.exports = mailModule;
--- route/api.js
+++ route/api.js
@@ -4,11 +4,53 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var EmailManager = require('./../module/EmailManager');
 //end lib
 
 //local var
 var m_fnGetUniStr = function(szTpoic){
 	return szTpoic + "_" + new Date().valueOf() + Math.round((Math.random()*1000));
+}
+var m_fnCreateMailContent = function(szCode,objOrder){
+	var option = {
+		receiver : objOrder.objAddress.name.value + "<" + objOrder.objAddress.email.value + ">",
+		topic : "蔬果溜溜訂單",
+		content : "",
+	};
+	//for title
+	option.content = option.content + "親愛的" + objOrder.objAddress.name.value + "您好，感謝您訂購蔬果溜溜<br>";
+	option.content = option.content + "總金額為： <span style=\"color:red\">$" + objOrder.nTotalPrice + "</span><br>";
+	option.content = option.content + "您的訂單編號為：" + szCode + "<br>您訂購資訊如下：";
+
+	//for css
+	var szTableCss = "<style>";
+	szTableCss = szTableCss + "table{width: 90%;text-align: center;margin: 15px auto;}";
+	szTableCss = szTableCss + "table,tr,td{border: 1px solid rgba(0,0,0,0.1);}";
+	szTableCss = szTableCss + "</style>";
+	option.content += szTableCss;
+
+	//for product table
+	var szHtmlCode = "<table border-collapse:\"collapse\" border=\"1\"><tr><td>商品名稱</td><td>商品單價</td><td>商品數量</td></tr>";
+	var szProductHtmlCode = "";
+	for( var i=0; i<objOrder.aryProduct.length; i++ ){
+		szProductHtmlCode = szProductHtmlCode + "<tr><td>" + objOrder.aryProduct[i].name + "</td><td>" + objOrder.aryProduct[i].price + "</td><td>" + objOrder.aryProduct[i].count + "</td></tr>";
+	}
+	szHtmlCode = szHtmlCode + szProductHtmlCode + "</table><br><br>";
+	option.content += szHtmlCode;
+
+	//for user info
+	var userinfoHtml = "<table>";
+	for( var key in objOrder.objAddress ){
+		userinfoHtml = userinfoHtml + "<tr>";
+		userinfoHtml = userinfoHtml + "<td>" + objOrder.objAddress[key].title + "</td>";
+		userinfoHtml = userinfoHtml + "<td style=\"font-weight:800;\">" + objOrder.objAddress[key].value + "</td>";
+		userinfoHtml = userinfoHtml + "</tr>";
+	}
+	userinfoHtml = userinfoHtml + "</table>";
+	option.content += userinfoHtml;
+
+	//return
+	return option;
 }
 //end local
 
@@ -30,10 +72,23 @@ app.route('/order')
 			//write file
 			fs.writeFile('orders.json', JSON.stringify(orders, null, '\t'), 'utf8', function (err) {
 				if (err) return console.log(err);
+
+				var option = m_fnCreateMailContent(szUniCode,order);
+				EmailManager.fnSendMail(option);
 				res.send(szUniCode);
 			}); // write it back 
 		});
 		
+	});
+app.route('/mail')
+	.get(function(req,res,next){
+		EmailManager.fnSendMail(function(err,info){
+			if(err){
+				res.send(err);
+			}
+
+			res.send(info);
+		});
 	});
 
 module.exports = app;
