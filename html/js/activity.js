32ce4c2e4cf1f907435bd027f3baf1daddfd873b
chpplen <chpplen@gmail.com>
2017-02-18 13:07:48 +0800

    add activities

--- html/js/activity.js
+++ html/js/activity.js
@@ -1,4 +1,17 @@
 //
 vegefruit66.controller('activityController', function($scope,$rootScope){
 	$rootScope.currentLink = "activity.html";
+
+	$scope.activitiesPages = {
+			main: "./activities/main.html",
+			activities1: "./activities/activity1.html",
+			activities2: "./activities/activity2.html",
+			activities3: "./activities/activity3.html",
+			activities4: "./activities/activity4.html",
+		};
+	$scope.now = $scope.activitiesPages['main']
+
+	$scope.pageChange = function(pageName) {
+		$scope.now = $scope.activitiesPages[pageName]
+	}
 });
